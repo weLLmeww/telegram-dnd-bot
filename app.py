b2989262f26016f75696588b69674a4a338ba058
weLLmeww <weLL.meww@gmail.com>
2025-06-20 17:16:56 +0300

    base ai added

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import os
 import asyncio
 
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart
+from aiogram import Bot, Dispatcher
 
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
@@ -14,7 +13,6 @@ ALLOWED_UPDATES = ['message']
 
 bot = Bot(token=os.getenv('TOKEN'))
 
-
 dp = Dispatcher()
 dp.include_routers(user_private_router, admin_private_router)
 
@@ -23,5 +21,5 @@ async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
-
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,14 +1,39 @@
+import os
+import openai
+
 from aiogram import types, Router
 from aiogram.filters import CommandStart
 
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+legend = os.getenv('LEGEND')
 user_private_router = Router()
 
+
+client = openai.OpenAI(
+    api_key = os.getenv('API_KEY'),
+    base_url = "https://api.intelligence.io.solutions/api/v1/"
+)
 
 
 @user_private_router.message(CommandStart())
 async def start_cmd(message: types.Message):
+    print("поступила команда старт")
     await message.answer("Это команда старт")
 
 @user_private_router.message()
-async def echo(message: types.Message):
-    await message.answer(message.text)
+async def handle_message(message: types.Message):
+    print(f"поступило сообщение {message.text}")
+    response = client.chat.completions.create(
+        model="deepseek-ai/DeepSeek-R1-0528",
+        messages=[
+            {"role": "system", "content": legend},
+            {"role": "user", "content": message.text},
+        ],
+        temperature=0.7,
+        stream=False,
+    )
+    answer = response.choices[0].message.content
+    print(answer)
+    await message.answer(answer)
